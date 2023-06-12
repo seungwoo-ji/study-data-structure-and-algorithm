7960f83d29862a06ee33a719440763bcc1492d1a
Seung Woo (Paul) Ji <wincow.ji@gmail.com>
2023-06-11 22:36:19 -0400

    Added optimized solution

--- array/49-group-anagrams.js
+++ array/49-group-anagrams.js
@@ -21,4 +21,29 @@ const groupAnagrams = (strs) => {
   return [...map.values()];
 };
 
-module.exports = { groupAnagrams };
+// hash map
+// time: O(M * N);
+// space: O(M * N)
+const groupAnagramsTwo = (strs) => {
+  if (strs.length === 1) return [[strs[0]]];
+
+  const map = new Map();
+
+  for (const word of strs) {
+    const key = new Array(26).fill(0);
+
+    for (const letter of word) {
+      const index = letter.charCodeAt(0) - 'a'.charCodeAt(0);
+      key[index]++;
+    }
+
+    const keyStr = key.toString();
+    const values = map.get(keyStr) || [];
+    values.push(word);
+    map.set(keyStr, values);
+  }
+
+  return [...map.values()];
+};
+
+module.exports = { groupAnagrams, groupAnagramsTwo };
--- array/49-group-anagrams.test.js
+++ array/49-group-anagrams.test.js
@@ -1,4 +1,4 @@
-const { groupAnagrams } = require('./49-group-anagrams');
+const { groupAnagrams, groupAnagramsTwo } = require('./49-group-anagrams');
 
 test('strs = ["eat","tea","tan","ate","nat","bat"] returns [["bat"],["nat","tan"],["ate","eat","tea"]]', () => {
   expect(groupAnagrams(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])).toEqual([
@@ -13,4 +13,19 @@ test('strs = [""] returns [[""]]', () => {
 });
 test('strs = ["a"] returns [["a"]]', () => {
   expect(groupAnagrams(['a'])).toEqual([['a']]);
+});
+
+test('strs = ["eat","tea","tan","ate","nat","bat"] returns [["bat"],["nat","tan"],["ate","eat","tea"]]', () => {
+  expect(groupAnagramsTwo(['eat', 'tea', 'tan', 'ate', 'nat', 'bat'])).toEqual([
+    ['eat', 'tea', 'ate'],
+    ['tan', 'nat'],
+    ['bat'],
+  ]);
+});
+
+test('strs = [""] returns [[""]]', () => {
+  expect(groupAnagramsTwo([''])).toEqual([['']]);
+});
+test('strs = ["a"] returns [["a"]]', () => {
+  expect(groupAnagramsTwo(['a'])).toEqual([['a']]);
 });
