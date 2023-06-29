85fb62cf8f7bcb8b0f94b644e0e23b6797e5d150
Seung Woo (Paul) Ji <wincow.ji@gmail.com>
2023-06-28 23:42:51 -0400

    Changed the solutions

--- dp/322-coin-change.js
+++ dp/322-coin-change.js
@@ -3,12 +3,13 @@
  * @param {number} amount
  * @return {number}
  */
+
 // recursive brute force using DFS
-// time: O(2^N)
+// time: O(M^N) where M is kinds of coins and N is amount
 // space: O(N)
 const coinChange = (coins, amount) => {
   // recursive function that finds the minimum number of coins to achieve the given amount
-  const findMin = (coins, coinIndex, amount) => {
+  const findMin = (coinIndex, amount) => {
     // base case
     if (coinIndex >= coins.length || amount <= 0) {
       return amount === 0 ? 0 : Infinity;
@@ -17,12 +18,11 @@ const coinChange = (coins, amount) => {
     let result;
 
     if (coins[coinIndex] > amount) {
-      const skipThisCoin = 0 + findMin(coins, coinIndex + 1, amount);
+      const skipThisCoin = 0 + findMin(coinIndex + 1, amount);
       result = skipThisCoin;
     } else {
-      const includeThisCoin =
-        1 + findMin(coins, coinIndex, amount - coins[coinIndex]);
-      const skipThisCoin = 0 + findMin(coins, coinIndex + 1, amount);
+      const includeThisCoin = 1 + findMin(coinIndex, amount - coins[coinIndex]);
+      const skipThisCoin = 0 + findMin(coinIndex + 1, amount);
       result = Math.min(includeThisCoin, skipThisCoin);
     }
 
@@ -30,46 +30,119 @@ const coinChange = (coins, amount) => {
     return result;
   };
 
-  const result = findMin(coins, 0, amount);
+  const result = findMin(0, amount);
   return result === Infinity ? -1 : result;
 };
 
-// dp - memoization (top-down approach)
-// time: O(N)
+// recursive DFS version two
+// time: O(M^N) where M is kinds of coins and N is amount
 // space: O(N)
 const coinChangeTwo = (coins, amount) => {
-  const findMin = (coins, coinIndex, amount, memo = {}) => {
+  const findMin = (amount) => {
+    if (amount === 0) return 0;
+
+    let count = Infinity;
+    for (const coin of coins) {
+      if (coin <= amount) {
+        count = Math.min(count, findMin(amount - coin) + 1);
+      }
+    }
+
+    return count;
+  };
+
+  const count = findMin(amount);
+  return count === Infinity ? -1 : count;
+};
+
+// dp - memoization (top-down approach)
+// time: O(M*N)
+// space: O(M*N)
+const coinChangeThree = (coins, amount) => {
+  const memo = Array.from(new Array(12 + 1), () => new Array(10000 + 1));
+
+  const findMin = (coinIndex, amount) => {
     if (coinIndex >= coins.length || amount <= 0) {
       return amount === 0 ? 0 : Infinity;
     }
 
-    if (memo[coins[coinIndex]]) return memo[coins[coinIndex]];
+    if (memo[coinIndex][amount] !== undefined) return memo[coinIndex][amount];
 
     let result;
 
     if (coins[coinIndex] > amount) {
-      const skipThisCoin = 0 + findMin(coins, coinIndex + 1, amount);
+      const skipThisCoin = 0 + findMin(coinIndex + 1, amount);
       result = skipThisCoin;
-      memo[coins[coinIndex]] = result;
+      memo[coinIndex][amount] = result;
     } else {
-      const includeThisCoin =
-        1 + findMin(coins, coinIndex, amount - coins[coinIndex]);
-      const skipThisCoin = 0 + findMin(coins, coinIndex + 1, amount);
+      const includeThisCoin = 1 + findMin(coinIndex, amount - coins[coinIndex]);
+      const skipThisCoin = 0 + findMin(coinIndex + 1, amount);
       result = Math.min(includeThisCoin, skipThisCoin);
-      memo[coins[coinIndex]] = result;
+      memo[coinIndex][amount] = result;
     }
 
     return result;
   };
 
-  const result = findMin(coins, 0, amount);
+  const result = findMin(0, amount);
+
   return result === Infinity ? -1 : result;
 };
 
-// dp - tabulation (bottom-up approach)
-// time: O(N)
+// dp - memoization (top-down approach) version two
+// time: O(M*N)
+// space: O(M*N)
+const coinChangeFour = (coins, amount) => {
+  const memo = new Map();
+  memo.set(0, 0);
+
+  const findMin = (amount) => {
+    if (memo.has(amount)) return memo.get(amount);
+
+    let count = Infinity;
+
+    for (const coin of coins) {
+      if (coin <= amount) {
+        count = Math.min(count, findMin(amount - coin) + 1);
+      }
+    }
+
+    memo.set(amount, count);
+    return count;
+  };
+
+  const count = findMin(amount);
+  return count === Infinity ? -1 : count;
+};
+
+// BFS
+// time: O(N*M)
 // space: O(N)
-const coinChangeThree = (coins, amount) => {
+const coinChangeFive = (coins, amount) => {
+  const queue = [0];
+  const set = new Set();
+  let depth = 0; // number of coins
+
+  while (queue.length > 0) {
+    const total = queue.shift();
+    if (total === amount) return depth;
+    if (set.has(total)) continue;
+    set.add(total);
+    for (const coin of coins) {
+      if (total + coin <= amount) {
+        queue.push(total + coin);
+        depth++;
+      }
+    }
+  }
+
+  return -1;
+};
+
+// dp - tabulation (bottom-up approach)
+// time: O(M*N)
+// space: O(N)
+const coinChangeSix = (coins, amount) => {
   const dp = new Array(amount + 1).fill(Infinity);
   dp[0] = 0;
 
@@ -85,4 +158,11 @@ const coinChangeThree = (coins, amount) => {
   return dp[amount] === Infinity ? -1 : dp[amount];
 };
 
-module.exports = { coinChange, coinChangeTwo, coinChangeThree };
+module.exports = {
+  coinChange,
+  coinChangeTwo,
+  coinChangeThree,
+  coinChangeFour,
+  coinChangeFive,
+  coinChangeSix,
+};
--- dp/322-coin-change.test.js
+++ dp/322-coin-change.test.js
@@ -2,6 +2,9 @@ const {
   coinChange,
   coinChangeTwo,
   coinChangeThree,
+  coinChangeFour,
+  coinChangeFive,
+  coinChangeSix,
 } = require('./322-coin-change');
 
 test('coins = [1,2,5] amount = 11 returns 3 (5 + 5 + 1)', () => {
@@ -38,4 +41,28 @@ test('coins = [2] amount = 3 returns -1 (not possible)', () => {
 
 test('coins = [1] amount = 0 returns 0', () => {
   expect(coinChangeThree([1], 0)).toBe(0);
+});
+
+test('coins = [2] amount = 3 returns -1 (not possible)', () => {
+  expect(coinChangeFour([2], 3)).toBe(-1);
+});
+
+test('coins = [1] amount = 0 returns 0', () => {
+  expect(coinChangeFour([1], 0)).toBe(0);
+});
+
+test('coins = [2] amount = 3 returns -1 (not possible)', () => {
+  expect(coinChangeFive([2], 3)).toBe(-1);
+});
+
+test('coins = [1] amount = 0 returns 0', () => {
+  expect(coinChangeFive([1], 0)).toBe(0);
+});
+
+test('coins = [2] amount = 3 returns -1 (not possible)', () => {
+  expect(coinChangeSix([2], 3)).toBe(-1);
+});
+
+test('coins = [1] amount = 0 returns 0', () => {
+  expect(coinChangeSix([1], 0)).toBe(0);
 });
